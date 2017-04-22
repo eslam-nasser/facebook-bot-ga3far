3d20d3a1901d0ab4d3187c43bdb9d027c2f03a57
eslamNasser <eslamnasser@hmaserv.net>
2017-04-22 18:13:47 +0200

    pushing

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ app.get('/webhook/', function (req, res) {
 
 // to post data
 app.post('/webhook/', function (req, res) {
-	let data = JSONbig.parse(req.body);
-	let messaging_events = data.entry[0].messaging
+	// let data = JSONbig.parse(req.body);
+	// let messaging_events = data.entry[0].messaging
+	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
 		let sender = event.sender.id.toString();
